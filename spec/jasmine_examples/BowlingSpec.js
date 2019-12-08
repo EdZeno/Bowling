375590c9551eebaa7ea07dbce927a45017719a84
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-12-08 16:46:03 +0000

    test is passing localy but fails in circleci

--- spec/jasmine_examples/BowlingSpec.js
+++ spec/jasmine_examples/BowlingSpec.js
@@ -1,10 +1,12 @@
 describe("createGame", function() {
   var createGame = require('../../lib/jasmine_examples/Bowling');
+  // const URLSearchParams = require('url-search-params-polyfill');
 
   it('should return an object', function() {
-  var params = new URLSearchParams("key1=value1&key2=value2");
+  // URLSearchParams(window.location.search);
+  var params = new URLSearchParams("file:///Users/eduardkulcsickij-gut/MAKERS/Bowling-challenge/Bowling/ui.html?F1R1=3&F1R2=8");
   console.log(createGame(params))
-  expect(createGame(params)).toEqual([['key1', 'value1'], ['key2', 'value2']])
+  expect(createGame(params)).toEqual([['F1R1', '3'], ['F1R2', '8']])
   })
 
 });
