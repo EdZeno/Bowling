65a56d98579b09f0f5dc8b77e46310a317e9ba84
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-12-10 15:11:58 +0000

    changed createGame function to return an array which is more convinient to loop trough

---
+++ spec/jasmine_examples/scoreSpec.js
@@ -0,0 +1,11 @@
+// describe('calculateScorescore', function() {
+//   let createGame = require('../../lib/jasmine_examples/Bowling');
+//   let calculateScore = require('../../lib/jasmine_examples/CalculateScore');
+//
+//   let params = new URLSearchParams("file:///Users/eduardkulcsickij-gut/MAKERS/Bowling-challenge/Bowling/ui.html?F1R1=5&F1R2=3");
+//
+//   it('it adds up the score in a single frame', function() {
+//     console.log(createGame(params))
+//   expect(calculateScore(createGame(params))).toEqual(8)
+//   })
+// })
