d4df1a18ab9458c638a82eb4f388c565041c2078
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-12-23 10:57:08 +0000

    adds up the score with a strike in it

--- spec/jasmine_examples/scoreSpec.js
+++ spec/jasmine_examples/scoreSpec.js
@@ -3,13 +3,13 @@ describe('calculateScorescore', function() {
   let calculateScore = require('../../lib/jasmine_examples/CalculateScore');
 
   let params = new URLSearchParams("file:///Users/eduardkulcsickij-gut/MAKERS/Bowling-challenge/Bowling/ui.html?F1R1=1&F1R2=1&F2R1=1&F2R2=1&F3R1=1&F3R2=1&F4R1=1&F4R2=1&F5R1=1&F5R2=1&F6R1=1&F6R2=1&F7R1=1&F7R2=1&F8R1=1&F8R2=1&F9R1=1&F9R2=1&F10R1=1&F10R2=1&F10R3=1");
-
-  // it('adds up the score in a single frame', function() {
-  //   // console.log(createGame(params))
-  // expect(calculateScore(createGame(params))).toEqual(5)
-  // })
-
   it('adds up the score in 10 frames without strikes and spares', function() {
   expect(calculateScore(createGame(params))).toEqual(21)
   })
+
+  let input_strike = new URLSearchParams("file:///Users/eduardkulcsickij-gut/MAKERS/Bowling-challenge/Bowling/ui.html?F1R1=10&F1R2=0&F2R1=1&F2R2=1&F3R1=1&F3R2=1&F4R1=1&F4R2=1&F5R1=1&F5R2=1&F6R1=1&F6R2=1&F7R1=1&F7R2=1&F8R1=1&F8R2=1&F9R1=1&F9R2=1&F10R1=1&F10R2=1&F10R3=1");
+  it('adds up the score in 10 frames with a strike', function() {
+  expect(calculateScore(createGame(input_strike))).toEqual(31)
+  })
+
 })
