b90bc913e94657cf509825d5a955d499983c3e7d
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-12-23 15:08:25 +0000

    can handel the 10th frame

--- spec/jasmine_examples/scoreSpec.js
+++ spec/jasmine_examples/scoreSpec.js
@@ -17,4 +17,9 @@ describe('calculateScorescore', function() {
   expect(calculateScore(createGame(input_strike_spare))).toEqual(40)
   })
 
+  let input_strike_spare_last_frame = new URLSearchParams("file:///Users/eduardkulcsickij-gut/MAKERS/Bowling-challenge/Bowling/ui.html?F1R1=10&F1R2=0&F2R1=1&F2R2=1&F3R1=4&F3R2=6&F4R1=1&F4R2=1&F5R1=1&F5R2=1&F6R1=1&F6R2=1&F7R1=1&F7R2=1&F8R1=1&F8R2=1&F9R1=1&F9R2=1&F10R1=10&F10R2=10&F10R3=10");
+  it('adds up the score in 10 frames with a strike and spare', function() {
+  expect(calculateScore(createGame(input_strike_spare_last_frame))).toEqual(67)
+  })
+
 })
