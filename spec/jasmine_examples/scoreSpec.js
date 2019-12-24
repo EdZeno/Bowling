dc3866a4b189ffd308124f884345f3c6932e86dd
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-12-24 10:27:09 +0000

    added a test for the 9th frame being a strike

--- spec/jasmine_examples/scoreSpec.js
+++ spec/jasmine_examples/scoreSpec.js
@@ -27,4 +27,9 @@ describe('calculateScorescore', function() {
     expect(calculateScore(createScoreCard(inputStrikeSpareLastFrame2))).toEqual(57)
   })
 
+  let inputStrikeFrame9 = new URLSearchParams("file:///Users/eduardkulcsickij-gut/MAKERS/Bowling-challenge/Bowling/ui.html?F1R1=10&F1R2=0&F2R1=1&F2R2=1&F3R1=4&F3R2=6&F4R1=1&F4R2=1&F5R1=1&F5R2=1&F6R1=1&F6R2=1&F7R1=1&F7R2=1&F8R1=1&F8R2=1&F9R1=10&F9R2=0&F10R1=10&F10R2=10&F10R3=10");
+  it('adds up the score in 10 frames with a strike in frame 9', function() {
+    expect(calculateScore(createScoreCard(inputStrikeFrame9))).toEqual(95)
+  })
+
 })
